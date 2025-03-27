954a88566c9e93a6c125b6706ac1cfc9a41da295
Scott Henley <scott.henley@pmg.com>
2025-03-27 15:45:12 -0500

    snacks finder

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -3,9 +3,11 @@ return {
   opts = {
     picker = {
       hidden = true, -- for hidden files
+      -- ignored = true,  -- for files in .gitignore
       sources = {
           files = {
             hidden = true, -- files and folders
+          -- ignored = true, -- files and folders
         },
       },
     },
